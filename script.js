d99d47a53b78112f9c18c59d3f05c52b6ce3288d
Sasha Granc <sasha82018201@gmail.com>
2022-02-03 06:38:14 +0300

    finished project

--- script.js
+++ script.js
@@ -1,40 +1,25 @@
 const downPayment = document.querySelector('.downPayment')
 const replenishment = document.querySelector('.replenishment')
-// const count = document.querySelector('.count')
 const interestRate = document.querySelector('.interestRate')
 const term = document.querySelector('.term')
 const submt = document.querySelector('.submt')
 const result = document.querySelector('.result')
-
-
-
-// A = P x (1 + r/n)nt, где:
-
-// A – сумма, которую вы получите в конце периода;
-
-// P – сумма первоначальных инвестиций, т.е. то, что вы вкладываете;
-
-// r – годовая ставка;
-
-// n – количество периодов начисления ставки (ежемесячно, ежеквартально, ежегодно и т.д.);
-
-// t – общий срок вклада в годах.
-
-
+const range = document.querySelector('#range')
+const rangeValue = document.querySelector('.rangeValue')
+rangeValue.innerHTML = `${range.value}`
+range.addEventListener('click', (event)=>{
+    rangeValue.innerHTML = `${range.value}`
+})
 submt.addEventListener('click',(event)=>{
     event.preventDefault()
-    let p = downPayment.value; //превоначальный взнос
-    console.log(p)
-    let r = interestRate.value; // процент
-    console.log(r)
-    // let n = count.value; 
-    let t = term.value //кол-во месяцев
-
-
-    let res = +p + ((p/100)*r)
-    console.log(res)
-    for(i = 1; i < t; i++){
-        let res2 = (res + +replenishment.value)+((p/100)*r)
-        console.log(res2)
+    result.innerHTML = "";
+    const arr = []
+    for(i=1; i<= +term.value +1;i++){
+        arr.push(i)
     }
+    const arrRed =  arr.reduce(function(sum, acum, index){
+        let procent = (((sum/100)* +interestRate.value)/12) 
+        result.innerHTML += `<p>${index} месяц ${(sum - +replenishment.value).toFixed(+range.value)}`
+        return sum + procent + +replenishment.value;
+    },+replenishment.value)
 })
