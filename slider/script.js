e14d6d8c5e26ccf1b2ebc322a1abee9b16ed6987
Александр <sasha82018201@gmail.com>
2022-03-14 19:34:01 +0300

    adding buttons

--- slider/script.js
+++ slider/script.js
@@ -1,27 +1,50 @@
-// const tittle = document.querySelector('.tittle')
-// const sweet = document.querySelector('.menu')
+let offset = 0
+const sliderPrev = document.querySelector('.slider-previous')
+const sliderNext = document.querySelector('.slider-next')
+const sliderLine = document.querySelector('.slider-line')
+const buttonNavigator = document.querySelector('.button-navigator')
+const navigator = document.querySelector('.navigator')
+const draw = (index)=>{
+    offset =+ index;
+    sliderLine.style.left = -offset + 'px';
+  
+}
 
-// tittle.addEventListener('click', ()=>{
-//     console.log('click')
-//     sweet.classList.toggle('oppen')
-// })
 
-let offset = 0 // смещение от левого края
-const slider = document.querySelector('.slider-line')
-document.querySelector('.slider-next').addEventListener('click', function(){
-    offset += 600;
-    if (offset > 3000) {
-        offset = 0;
-    }
-    slider.style.left = -offset + 'px'
+const init = ()=>{
+    buttonNavigator.addEventListener('click', (event)=>{
         
-})
+        btnId = event.target.id
+        // console.log(btnClass)
+        console.log(btnId)
+        draw(btnId)
+    })
+    sliderPrev.addEventListener('click',()=>{
+        draw(offset-600)
+        if (offset < 0) {
+            draw(3000);
+        }
+    })
+    sliderNext.addEventListener('click',()=>{
+        draw(offset+600)
+        if(offset > 3000){
+            draw(0)
+        }
+    })
+}
+if(offset = 0){
 
-document.querySelector('.slider-previous').addEventListener('click', function(){
-    offset = offset - 600;
-    if (offset < 0) {
-            offset = 3000;
-    }
-    slider.style.left = -offset + 'px'
-        
-})
+}
+init()
+
+// let menu = document.querySelectorAll('menu')
+// console.log(menu)
+
+// const init = ()=>{
+//     menu.forEach((item)=>
+//         item.classList.remove('active'))
+//         this.classList.add('active')   
+// }
+
+// menu.forEach((item) =>
+// item.addEventListener('click', init))
